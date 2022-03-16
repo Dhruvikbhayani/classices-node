28f4329576e55041db35698286711f3526f25db9
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-16 16:21:26 +0530

    16-03

--- file/httpfile.js
+++ file/httpfile.js
@@ -1,21 +1,21 @@
 const http = require('http')
 const fs = require('fs')
 
-let app = http.createServer((req,res)=>{
+let app = http.createServer((req, res) => {
 
-    fs.readFile("xyz.txt","utf8",function(err,data){
-        if(err){
+    fs.readFile("xyz.txt", "utf8", function (err, data) {
+        if (err) {
             res.statusCode(500)
             res.write("unable to read")
             res.end();
         }
-        else{
+        else {
             res.write(data);
             res.end();
         }
     });
 });
 
-app.listen(5020,()=>{
+app.listen(5020, () => {
     console.log("listing post5020")
 })
