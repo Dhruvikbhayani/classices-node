7ad4f9d19aed6987d958057fb1d8a465c8f2c166
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-16 15:47:10 +0530

    16-03-2022

--- file/http.js
+++ file/http.js
@@ -6,10 +6,10 @@ var app = http.createServer((req, res) => {
     res.write("Hello");
     res.write("<h1>Course</h1>")
     res.write("<h1>nodejs</h1>")
-    res.write("request Url" + req.url);
-    res.write("request method" + req.method)
+    res.write("request Url/" + req.url);
+    res.write("//request method" + req.method)
     res.end();
 });
-app.listen(5000, () => {
+app.listen(5500, () => {
     console.log("lisiting 5050")
 });
---
+++ file/httpfile.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const fs = require('fs')
+
+let app = http.createServer((req,res)=>{
+    fs.readFile("xyz.txt","utf8",function(err,data){
+        if(err){
+            res.statusCode(500)
+            res.write("unable to read")
+            res.end()
+        }
+        else{
+            res.write(data);
+            res.end
+        }
+    });
+});
+
+app.listen(5020,()=>{
+    console.log("listing post5020")
+})
