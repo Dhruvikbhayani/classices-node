79957aff8989b509426914753883881b0f3626be
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-17 17:48:00 +0530

    17-03-22

--- mongoose/insert.js
+++ mongoose/insert.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose')
 
-const emschema= new mongoose.Schema({_id:Number,name:String,age:Number,address:String},{versionKey:false});
+const emschema= new mongoose.Schema({name:String,age:Number,address:String});
 const emmodel=mongoose.model("employee",emschema)
 mongoose.connect("mongodb://localhost/employee")
-var newemp=new emmodel({_id:1,name:"Dhruvik",age:22,address:"surat"});
+var newemp=new emmodel({name:"Dhruvik",age:22,address:"surat"});
 newemp.save(after);
 function after(err){
     if(err){
