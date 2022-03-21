f663711f6583bbac01f54b5476f3ddb4f8ff0a46
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-21 15:53:16 +0530

    21-03-2022

---
+++ file/httpserver.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const url= require('url')
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{"content-type":"text/html"})
+    const qureryobject=url.parse(req.url,true).query
+    console.log(qureryobject)
+    const uname=qureryobject.uname
+    const upwd=qureryobject.upwd
+    uname==="skill" && upwd==="qode"?res.write("<h1>login success</h1>"): res.write("<h1>login fille</h1>")
+    res.end()
+})
+
+server.listen(5050,()=>{
+    console.log("This port is run")
+})
--- mongoose/insert.js
+++ mongoose/insert.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 const emschema= new mongoose.Schema({name:String,age:Number,address:String});
 const emmodel=mongoose.model("employee",emschema)
 mongoose.connect("mongodb://localhost/employee")
-var newemp=new emmodel({name:"Dhruvik",age:22,address:"surat"});
+var newemp=new emmodel({name:"jainam",age:55,address:"ahmedabad"});
 newemp.save(after);
 function after(err){
     if(err){
