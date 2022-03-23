ad97362cd58c03bc719dc98622d7a5e52827dd0c
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-23 16:01:36 +0530

    23-03-22

---
+++ http/posthttp.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const querystring = require('querystring')
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "content-type": "text/html" })
+    if (req.method == "post") {
+        var postparametes = "";
+        req.on("data", function() {
+            postparametes += data;
+        });
+        req.on("end", function() {
+            var postdata = querystring.parse(postparametes)
+            console.log(postdata)
+            if (postdata['uname'] == "skill" && postdata["upwd"] == "qode") {
+                res.write("<h1>Login success</h1>")
+            } else {
+                res.write("<h1>login fail</h1>")
+            }
+        })
+    }
+})
+server.listen(5050, () => {
+    console.log("server is run")
+})
