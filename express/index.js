40655076b0e7815e55f432533cc2706be30c6705
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-24 15:29:48 +0530

    24-03-22

--- express/index.js
+++ express/index.js
@@ -7,10 +7,10 @@ app.get("/home", function(req, res) {
     res.write("Hello")
     res.end();
 
-    console.log("req.url", req.url)
-    console.log("req.method", req.method)
-    console.log("req.headers", req.headers)
-    console.log("req.query", req.query)
-    console.log("req.body", req.body)
+    // console.log("req.url", req.url)
+    // console.log("req.method", req.method)
+    // console.log("req.headers", req.headers)
+    // console.log("req.query", req.query)
+    // console.log("req.body", req.body)
 
 })
