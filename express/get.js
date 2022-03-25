ff04feb59f394ce13e0e32af4be69ac295ec6dec
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-25 17:06:53 +0530

    25-03-22

--- express/get.js
+++ express/get.js
@@ -1,16 +1,17 @@
-// http://localhost:5070/?uname=skill&upwd=qode
-const http = require('http')
+// http://localhost:8400/?uname=skill&upwd=qode
+const express = require("express")
 const url = require('url')
-const server = http.createServer((req, res) => {
+const app = express()
+app.listen(8400, () => {
+    console.log(`this port is run 8400`)
+})
+app.get("/", (req, res) => {
     res.writeHead(200, { "content-type": "text/html" })
+    res.sendFile(__dirname + "/form.html")
     const qureryobject = url.parse(req.url, true).query
     console.log(qureryobject)
     const uname = qureryobject.uname
     const upwd = qureryobject.upwd
     uname === "skill" && upwd === "qode" ? res.write("<h1>login success</h1>") : res.write("<h1>login file</h1>")
     res.end()
-})
-
-server.listen(5070, () => {
-    console.log("This port is run")
 })
