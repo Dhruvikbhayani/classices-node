e99ed5dc54cb5e0a20f68d8427690a96e7fda03d
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-03-26 09:10:16 +0530

    26-03-22

--- express/get.js
+++ express/get.js
@@ -6,12 +6,11 @@ app.listen(8400, () => {
     console.log(`this port is run 8400`)
 })
 app.get("/", (req, res) => {
-    res.writeHead(200, { "content-type": "text/html" })
-    res.sendFile(__dirname + "/form.html")
-    const qureryobject = url.parse(req.url, true).query
-    console.log(qureryobject)
-    const uname = qureryobject.uname
-    const upwd = qureryobject.upwd
-    uname === "skill" && upwd === "qode" ? res.write("<h1>login success</h1>") : res.write("<h1>login file</h1>")
+    res.sendFile(__dirname + "form.html")
+})
+app.get("/login", (req, res) => {
+    var name = req.query.uname
+    var password = req.query.upwd
+    name === "skill" && password === "qode" ? res.write("<h1>login success</h1>") : res.write("<h1>login file</h1>")
     res.end()
 })
