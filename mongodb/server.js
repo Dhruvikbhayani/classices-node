bd660280fa10100dc3abbf4de87e42e85a5ac395
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-01 14:01:47 +0530

    01-04-21

--- mongodb/server.js
+++ mongodb/server.js
@@ -4,6 +4,9 @@ const app = express()
 const data = require("./data")
 const user = require('./usermodel')
 const port = 2021
+const bodyparase = require('body-parser')
+const { use } = require('express/lib/application')
+app.use(bodyparase.json())
 app.use(express.json())
     // const cors = require('cors')
     // app.use(cors())
@@ -18,23 +21,31 @@ app.get("/insert", (req, res) => {
     // const result = user.insertMany(data.users)
     // res.send(result)
 
-    var a = user.Save().then((err, data) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(data)
-        }
-    })
-    res.send(a)
-    mongoose.disconnect()
+    var users = new user(req.query)
+    users.save().then((err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(data)
+            }
+        })
+        //mongoose.disconnect()
 })
 
-// app.get("/insert", (req, res) => {
-//     // const result = user.insertMany(data.users)
-//     // res.send(result)
-//     mongoose.disconnect()
-// })
+app.post("/insert", (req, res) => {
+    // const result = user.insertMany(data.users)
+    // res.send(result)
 
+    var users = new user(req.body)
+    users.save().then((err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(data)
+            }
+        })
+        //mongoose.disconnect()
+})
 app.listen(port, () => {
     console.log(`srever is running port ${port}`)
 })
