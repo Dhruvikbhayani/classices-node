41f3a6b51aeb5c8e68b3c496cc6633233596f383
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-11 15:24:19 +0530

    11-04-21

---
+++ multer/multer.js
@@ -0,0 +1,22 @@
+const express = require('express')
+var multer = require('multer')
+var app = express()
+
+var filestorange = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '_' + Date.now() + file.originalname)
+    }
+})
+var uploads = multer({ storage: filestorange })
+
+app.post("/file", uploads.single("images"), (req, res) => {
+    res.json({ "msg": "fileuploade" })
+})
+
+
+app.listen(5010, (req, res) => {
+    console.log("server is run")
+})
