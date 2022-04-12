26047ae30ffebb317ee17e2b7ed15085baebdfe9
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-12 15:10:45 +0530

    12-04-2022

---
+++ ejs/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    var model = {
+        studentid: "501",
+        studentname: "Dhruvik",
+        studentmarks: "60",
+        subject: [
+            { name: "math", marks: "33" },
+            { name: "sci", marks: "33" },
+            { name: "english", marks: "55" }
+        ]
+    }
+    res.render('student', model)
+})
+
+
+app.listen(8010, (req, res) => {
+    console.log("server is run")
+})
