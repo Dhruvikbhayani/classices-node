e1a64d4b703d8a553292612786f65304aff361a2
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-18 15:43:56 +0530

    18-04-22

---
+++ CRUD/getmethod.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const module1 = express.Router()
+const mongodb = require('mongodb')
+const skill = mongodb.MongoClient
+
+module1.get("/", (req, res) => {
+    skill.connect(process.env.CONNECTION_URL, (err, connections) => {
+        if (err) throw err;
+        else {
+            const db = connections.db(process.env.DATABASE_NAME, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+            console.log(db)
+            db.collection(process.env.COLLECTION_NAME).find().toArray((err, data) => {
+                if (err) throw err
+                else {
+                    res.send(data)
+                }
+            })
+        }
+    })
+})
+
+
+
+
+
+module.exports = module1
---
+++ CRUD/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.json())
+
+const module1 = require('./getmethod')
+app.use("/product", module1)
+
+const port = process.env.PORT || 5010
+app.listen(port, () => {
+    console.log(`srever is run ${port}`)
+})
+
+//mongodb+srv://DhruvikBhayani:<password>@dhruvik.0qfcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//mongodb+srv://DhruvikBhayani:admin@dhruvik.0qfcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
