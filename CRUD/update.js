0e491c6865230f83f391acd47cc1f57bf19b64ab
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-21 15:43:49 +0530

    21-04-22

--- CRUD/update.js
+++ CRUD/update.js
@@ -4,30 +4,25 @@ const module3 = express.Router()
 const skill = mongodb.MongoClient
 
 module3.put("/", (req, res) => {
-    skill.connect(process.env.CONNECTION_URL, (err, connections) => {
+    const connections = req.db
+    const db = connections.db(process.env.DATABASE_NAME, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    db.collection(process.env.COLLECTION_NAME).updateOne({ "name": req.body.name }, {
+        $set: {
+            "name": req.body.name,
+            "age": req.body.age,
+            "email": req.body.email
+        }
+
+    }, (err, result) => {
         if (err) throw err;
         else {
-            const db = connections.db(process.env.DATABASE_NAME, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            })
-            db.collection(process.env.COLLECTION_NAME).updateOne({ "name": req.body.name }, {
-                $set: {
-                    "name": req.body.name,
-                    "age": req.body.age,
-                    "email": req.body.email
-                }
-
-            }, (err, result) => {
-                if (err) throw err;
-                else {
-                    res.send("Data is update")
-                    console.log(db)
-                }
-
-            })
+            res.send("Data is update")
+            console.log(db)
         }
-    })
 
+    })
 })
 module.exports = module3
