12215946e84b38a62c2102c3e0a6370a41baf8cc
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-04-21 15:45:26 +0530

    21-04-2022

--- CRUD/delete.js
+++ CRUD/delete.js
@@ -4,23 +4,20 @@ const module4 = express.Router()
 const skill = mongodb.MongoClient
 
 module4.delete("/", (req, res) => {
-    skill.connect(process.env.CONNECTION_URL, (err, connections) => {
+    const connections = req.db
+    const db = connections.db(process.env.DATABASE_NAME, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    db.collection(process.env.COLLECTION_NAME).deleteOne({ "name": req.body.name }, (err, result) => {
         if (err) throw err;
         else {
-            const db = connections.db(process.env.DATABASE_NAME, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            })
-            db.collection(process.env.COLLECTION_NAME).deleteOne({ "name": req.body.name }, (err, result) => {
-                if (err) throw err;
-                else {
-                    res.send("Data is delete")
-                    console.log(db)
-                }
-
-            })
+            res.send("Data is delete")
+            console.log(db)
         }
+
     })
+
 
 })
 module.exports = module4
--- CRUD/postmethod.js
+++ CRUD/postmethod.js
@@ -12,21 +12,18 @@ module2.post("/", (req, res) => {
         "age": req.body.age,
         "email": req.body.email
     }
-    skill.connect(process.env.CONNECTION_URL, (err, connections) => {
-        if (err) throw err;
+    const connections = req.db
+    const db = connections.db(process.env.DATABASE_NAME, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    db.collection(process.env.COLLECTION_NAME).insertOne(data, (err, result) => {
+        if (err) throw err
         else {
-            const db = connections.db(process.env.DATABASE_NAME, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            })
-            db.collection(process.env.COLLECTION_NAME).insertOne(data, (err, result) => {
-                if (err) throw err
-                else {
-                    res.send("data is insert")
+            res.send("data is insert")
 
-                }
-            })
         }
     })
+
 })
 module.exports = module2
